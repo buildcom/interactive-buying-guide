1b4ff9bf250f2c2b3f3601d23d19f74f1deef060
novacourtois <courtois1337@gmail.com>
2015-04-24 15:56:12 -0500

    got it working!

--- js/app.js
+++ js/app.js
@@ -1,168 +1,181 @@
-(function(){
+(function() {
 
-  angular.module('productGuide',['ngRoute','ngAnimate'])
-  .config(['$routeProvider',
-    function($routeProvider) {
-      $routeProvider
-        .when('/item/:itemId', {
-          templateUrl: 'views/pdp.html',
-          controller: 'pdpCtrl'
-        })
-        .when('/:n0?/:n1?/:n2?/:n3?/:n4?/:n5?/:n6?/:n7?/:n8?/:n9?/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      });
-  }])
-  .controller('MainCtrl', function($scope, $http, $routeParams) {
+    angular.module('productGuide', ['ngRoute', 'ngAnimate'])
+        .config(['$routeProvider',
+            function($routeProvider) {
+                $routeProvider
+                    .when('/:n0?/:n1?/:n2?/:n3?/:n4?/:n5?/:n6?/:n7?/:n8?/:n9?/', {
+                        templateUrl: 'views/main.html',
+                        controller: 'MainCtrl'
+                    });
+            }
+        ])
+        .controller('MainCtrl', function($scope, $http, $routeParams, $location) {
 
-    console.log('route parameters');
-    console.log($routeParams);
+            $scope.itemSelection = [];
+            $scope.items = false;
 
-    $scope.params = $routeParams;
-    // $scope.productSelection = step.options;
-    $scope.productSelection = [{"title":"Large Room"},{"title":"Medium Room"},{"title":"Small Room"}];
-    $scope.prompt = step.question;
+            $scope.getNextSet = function(o, index) {
+                if (o.products.length == 0) {
+                    $scope.items = false;
+                    $location.path($location.path() + (index +1)+ '/');
+                } else {
 
-    n_params = Object.keys($scope.params).length;
+                    $scope.items = true;
 
-    $scope.data = {};
+                    $location.path($location.path() + (index +1)+ '?items=true');
 
-    var results = {
-      "question": {
-        "options": [
-            {
-                "products": [],
-                "answer": {},
-                "step": {'question': {'text': '', 'options': []}},
-                "text": "Cool my space"
-            },
-            {
-                "products": [],
-                "answer": {},
-                "step": {'question': {'text': '', 'options': []}},
-                "text": "Get rid of excess moisture"
-            },
-            {
-                "text": "Both",
-                "products": [],
-                "answer": {},
-                "step": {
-                    "question": {
-                        "text": "How humid is your space?",
-                        "options": [
-                            {
-                                "text": "Moderately Damp - smell musty in humid weather",
-                                "products": [],
-                                "answer": {},
-                                "step": {
-                                    "question": {
-                                        "text": "What size space are you looking to dehumidify?",
-                                        "options": [
-                                            {
+                    $scope.itemSelection = o.products;
+                }
+            };
+
+            $scope.params = $routeParams;
+
+            var results = {
+                "question": {
+                    "options": [{
+                        "products": [],
+                        "answer": {},
+                        "step": {
+                            'question': {
+                                'text': '',
+                                'options': []
+                            }
+                        },
+                        "text": "Cool my space"
+                    }, {
+                        "products": [],
+                        "answer": {},
+                        "step": {
+                            'question': {
+                                'text': '',
+                                'options': []
+                            }
+                        },
+                        "text": "Get rid of excess moisture"
+                    }, {
+                        "text": "Both",
+                        "products": [],
+                        "answer": {},
+                        "step": {
+                            "question": {
+                                "text": "How humid is your space?",
+                                "options": [{
+                                    "text": "Moderately Damp - smell musty in humid weather",
+                                    "products": [],
+                                    "answer": {},
+                                    "step": {
+                                        "question": {
+                                            "text": "What size space are you looking to dehumidify?",
+                                            "options": [{
                                                 "products": [
-                                                    "DEP301EW",
-                                                    "DDR30A2GP",
-                                                    "FAD301NWD"
+                                                    {"sku":"DEP301EW", "description":"", "link":"", "img":"", "name":""},
+                                                    {"sku":"DDR30A2GP", "description":"", "link":"", "img":"", "name":""},
+                                                    {"sku":"FAD301NWD", "description":"", "link":"", "img":"", "name":""}
                                                 ],
                                                 "answer": {},
-                                                "step": {'question': {'text': '', 'options': []}},
+                                                "step": {
+                                                    'question': {
+                                                        'text': '',
+                                                        'options': []
+                                                    }
+                                                },
                                                 "text": "Small space - under 400 sq ft"
-                                            },
-                                            {
+                                            }, {
                                                 "products": [
                                                     "DEP501EW",
                                                     "DDR50A2GP",
                                                     "DDR45A3GP"
                                                 ],
                                                 "answer": {},
-                                                "step": {'question': {'text': '', 'options': []}},
+                                                "step": {
+                                                    'question': {
+                                                        'text': '',
+                                                        'options': []
+                                                    }
+                                                },
                                                 "text": "Medium Space - Up to 1000 sq ft"
-                                            },
-                                            {
+                                            }, {
                                                 "products": [
-                                                    "DEP701EW",
-                                                    "DDR60A2GP",
-                                                    "DDR70A2GP"
+                                                    {"sku":"DEP701EW", "description":"", "link":"", "img":"", "name":""},
+                                                    {"sku":"DDR60A2GP", "description":"", "link":"", "img":"", "name":""},
+                                                    {"sku":"DDR70A2GP", "description":"", "link":"", "img":"", "name":""}
                                                 ],
+
+
                                                 "answer": {},
-                                                "step": {'question': {'text': '', 'options': []}},
+                                                "step": {
+                                                    'question': {
+                                                        'text': '',
+                                                        'options': []
+                                                    }
+                                                },
                                                 "text": "Large space - 1000 sq ft +"
-                                            }
-                                        ]
+                                            }]
+                                        }
                                     }
-                                }
-                            },
-                            {
-                                "products": [],
-                                "answer": {},
-                                "step": {'question': {'text': '', 'options': []}},
-                                "text": "Damp - consistently smell musty"
-                            },
-                            {
-                                "products": [],
-                                "answer": {},
-                                "step": {'question': {'text': '', 'options': []}},
-                                "text": "Very Damp - Consistently musty and may have mold or moisture spots in areas"
+                                }, {
+                                    "products": [],
+                                    "answer": {},
+                                    "step": {
+                                        'question': {
+                                            'text': '',
+                                            'options': []
+                                        }
+                                    },
+                                    "text": "Damp - consistently smell musty"
+                                }, {
+                                    "products": [],
+                                    "answer": {},
+                                    "step": {
+                                        'question': {
+                                            'text': '',
+                                            'options': []
+                                        }
+                                    },
+                                    "text": "Very Damp - Consistently musty and may have mold or moisture spots in areas"
+                                }]
                             }
-                        ]
+                        }
+                    }],
+                    "text": "Are you looking to cool your space or get rid of excess moisture?"
+                }
+            };
+
+            var display_data = results.question;
+            $scope.question = display_data.text;
+            $scope.productSelection = display_data.options;
+
+            n_params = Object.keys($scope.params).length;
+
+            if (n_params) {
+                results = results.question;
+
+                for (var i = 1; i < n_params + 1; ++i) {
+                    if( i == n_params) {
+
+                        var pars = $scope.params['n'+(n_params-1)].split("?");
+
+                        if(pars.length > 1) {
+                            $scope.items = true;
+                            tmp_index = 'n' + (n_params-1);
+                            $scope.itemSelection = results.options[parseInt(pars[0]) - 1].products;
+                        }
+                        else {
+                            results = results.options[$scope.params[('n' + (i -1 ))] - 1].step.question;
+                            $scope.items = false;
+                        }
                     }
+                    else {
+                        results = results.options[$scope.params[('n' + (i - 1))] - 1].step.question;
+                        $scope.items = false;
+                    }       
+                }
+                if (!$scope.items) {
+                    $scope.question = results.text;
+                    $scope.productSelection = results.options;    
                 }
             }
-        ],
-        "text": "Are you looking to cool your space or get rid of excess moisture?"
-        }
-    };
-
-    var display_data = results.question;
-    console.log(display_data);
-
-    $scope.prompt = display_data.text;
-    $scope.productSelection = display_data.options;
-    
-    if (n_params) {
-      console.log('number of parameters: '+n_params);
-
-      console.log($scope.params[('n'+(n_params-1))]);
-      for(var i = 0; i < n_params; ++i) {
-        console.log(i);
-      }
-    }
-
-
-
-
-    $scope.getNextSet = function(o) {
-      console.log(o);
-      // $scope.productSelection = o.options;
-      // $scope.prompt = o.question;
-      
-      if ("step.options" in o) {
-        $scope.productSelection = o.options;
-      }
-      else {
-        console.log('Nothing to show!');
-      }
-    };
-
-  })
-.controller('pdpCtrl', function($scope, $routeParams) {
-  // console.log('this');
-  console.log('hello');
-    console.log($routeParams);
-
-    $scope.params = $routeParams;
-    // $scope.productSelection = step.options;
-    $scope.productSelection = [{"title":"Large Room"},{"title":"Medium Room"},{"title":"Small Room"}];
-    $scope.prompt = step.question;
-
-    n = Object.keys($scope.params).length;
-
-    console.log($scope.params[('n'+(n-1))]);
-
-});
-
-var step = {
-  
-};
+        });
 
 })();
